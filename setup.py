307cb9d666266c5b4783b326229ae163f0c87dfc
Chrisjan Wüst <chrisjanwust@gmail.com>
2024-07-10 19:30:27 +0200

    bring in new setup.py changes in order to resolve conflicts

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
+with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name="imagebind",
-    version="0.1.0",
+    name='imagebind',
+    version='0.1.0',
     packages=find_packages(),
     package_data={
-        "imagebind": ["bpe/bpe_simple_vocab_16e6.txt.gz"],
+        'imagebind': ['bpe/bpe_simple_vocab_16e6.txt.gz'],
     },
-    description="A brief description of the package",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/facebookresearch/ImageBind",
+    description='A brief description of the package',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/facebookresearch/ImageBind',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International",
+        'Programming Language :: Python :: 3',
+        'License :: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International',
     ],
     install_requires=required,
-    dependency_links=["https://download.pytorch.org/whl/cu113"],
+    dependency_links=['https://download.pytorch.org/whl/cu113'],
 )
