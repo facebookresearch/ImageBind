7d68fc308205d3d013721b91798c951ed59afe06
Ahmed Saed <37080003+Ahmedsaed@users.noreply.github.com>
2025-02-25 21:51:37 +0000

    Update deprecated modules

--- imagebind/data.py
+++ imagebind/data.py
@@ -17,7 +17,7 @@ from pytorchvideo import transforms as pv_transforms
 from pytorchvideo.data.clip_sampling import ConstantClipsPerVideoSampler
 from pytorchvideo.data.encoded_video import EncodedVideo
 from torchvision import transforms
-from torchvision.transforms._transforms_video import NormalizeVideo
+from torchvision.transforms import Normalize
 
 from imagebind.models.multimodal_preprocessors import SimpleTokenizer
 
